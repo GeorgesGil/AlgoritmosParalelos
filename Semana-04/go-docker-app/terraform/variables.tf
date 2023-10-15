ade5f4406f788f306388caeb2b0e4acdc6a8d92e
Georges <georgesgil22@gmail.com>
2023-10-15 18:34:17 -0400

    Prueba 1

--- Semana-04/go-docker-app/terraform/variables.tf
+++ Semana-04/go-docker-app/terraform/variables.tf
@@ -7,29 +7,16 @@ variable "token" {
 variable "ssh_fingerprint" {
     type        = string
     description = "Public Key"
-    default     = "c2:aa:fb:5a:cc:10:26:db:1f:ea:f2:5e:e1:73:30:80"
+    default     = "8c:d7:eb:fc:4c:be:46:c7:a0:04:18:90:18:a3:f0:0c"
 }
 
+variable "public_key" {
+    type = string
+    description = "Public Key"
+    default = "C:\\Users\\elbac\\.ssh\\id_rsa.pub"
+}
 variable "private_key" {
-    type        = string
+    type = string
     description = "Private Key"
-    default     = file("${path.module}/putty/private.ppk")
-}
-
-variable "DB_USER" {
-    type        = string
-    description = "Database User"
-    default     = "georgesgil"
-}
-
-variable "DB_PASSWORD" {
-    type        = string
-    description = "Database Password"
-    default     = "1182363"
-}
-
-variable "DB_NAME" {
-    type        = string
-    description = "Database Name"
-    default     = "georgesgil"
+    default =  "C:\\Users\\elbac\\.ssh\\id_rsa"
 }
